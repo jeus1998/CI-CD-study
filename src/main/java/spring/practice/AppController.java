8b7f97f7ef59a84225ee1a614ed162d5c6f38cfe
baejeu <zeus20240228@gmail.com>
2025-03-25 19:05:16 +0900

    [2025:03:25] CodeDeploy test

--- src/main/java/spring/practice/AppController.java
+++ src/main/java/spring/practice/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
     @GetMapping("/")
     public String index(){
-        return "Hello Github Actions with SCP";
+        return "Hello Github Actions with CodeDeploy & S3";
     }
 }
